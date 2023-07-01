092fba4a5f664036c4b07eef301f264f8da5e918
Sito8943 <sito8943@gmail.com>
2023-07-01 12:01:33 -0700

    fixed responsive slide

---
+++ src/views/Projects/Project.jsx
@@ -0,0 +1,40 @@
+import { useMemo } from "react";
+
+// contexts
+import { useLanguage } from "../../contexts/LanguageProvider";
+
+// font awesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExternalLink } from "@fortawesome/free-solid-svg-icons";
+
+function Project({ title, id, body, image, url, buttonClass }) {
+  const { languageState } = useLanguage();
+
+  const { buttons } = useMemo(
+    () => ({
+      buttons: languageState.texts.buttons,
+    }),
+    [languageState]
+  );
+
+  return (
+    <div className={`cell ${id}`}>
+      <img src={image} alt={`${id} header image`} className="image" />
+      <div className="info">
+        <h1 className="text-[white]">{title}</h1>
+        <p className="text-[white] w-[80%]">{body}</p>
+        <a
+          href={url}
+          target="_blank"
+          rel="noreferrer"
+          className={`go-button ${buttonClass}`}
+        >
+          {buttons.visit}
+          <FontAwesomeIcon className="text-sm mt-[1px]" icon={faExternalLink} />
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Project;
--- src/views/Projects/Projects.jsx
+++ src/views/Projects/Projects.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 
 import { css } from "@emotion/css";
 
@@ -9,14 +9,20 @@ import { faArrowRight, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 // styles
 import "./styles.css";
 
+// contexts
+import { useLanguage } from "../../contexts/LanguageProvider";
+
 // components
-import DescubreTrinidad from "./DescubreTrinidad";
-import AltaVoz from "./AltaVoz";
+import Project from "./Project";
 
 function Projects() {
+  const { languageState } = useLanguage();
+
   const [positionX, setPositionX] = useState(0);
 
-  const columns = 2;
+  const columns = useMemo(() => {
+    return languageState.texts.projects.length;
+  }, [languageState]);
 
   const goRight = useCallback(() => {
     if (positionX !== -1 * 100 * (columns - 1)) setPositionX(positionX - 100);
@@ -25,6 +31,12 @@ function Projects() {
   const goLeft = useCallback(() => {
     if (positionX !== 0) setPositionX(positionX + 100);
   }, [positionX]);
+
+  const printProjects = useCallback(() => {
+    return languageState.texts.projects.map((project) => (
+      <Project {...project} key={project.id} />
+    ));
+  }, [languageState]);
 
   return (
     <section id="projects" className="w-full h-screen relative">
@@ -45,11 +57,11 @@ function Projects() {
       </div>
       <div
         className={`main ${css({
+          gridTemplateColumns: `repeat(${columns}, 100vw)`,
           transform: `translateX(${positionX}vw)`,
         })}`}
       >
-        <DescubreTrinidad />
-        <AltaVoz />
+        {printProjects()}
       </div>
     </section>
   );
